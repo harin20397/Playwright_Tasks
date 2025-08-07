651fa208f2c2959f3aeed76a543b8b7ea60c68b3
Harin Patel <harinpatel@Harrys-Mac-mini.local>
2025-08-07 14:58:38 +0530

    initial commit of basics.js with console logs and variable type checks

---
+++ basics.js
@@ -0,0 +1,16 @@
+console.log("Hello, World!");
+
+let a = 5;
+console.log(typeof(a));
+
+let b = "Hello";
+console.log(typeof(b));
+
+let c = "Harin Patel"
+console.log(typeof(c));
+
+let required = true;
+console.log(typeof(required));
+
+console.log(required);
+console.log(!required);
