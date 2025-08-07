a5dc3dcb94539f1d0f630dd170b8c3e01bb7fa2e
Harin Patel <harinpatel@Harrys-Mac-mini.local>
2025-08-07 19:06:31 +0530

    add basics1.js and basics2.js with examples of variable types, control structures, and loops

---
+++ basics2.js
@@ -0,0 +1,33 @@
+// If-else statement example
+const flag = true;
+
+if (!flag)
+{
+    console.log("Conditions is satisfied");
+}
+else
+{
+    console.log(flag);
+    console.log("Conditions is not satisfied");
+}
+
+// While loop example
+let i = 0;
+while (i > 10)
+{
+    i++;
+    console.log(i);
+}
+
+// Do-while loop example
+do
+{
+    i++;
+} while (i < 15);
+console.log(i);
+
+// For loop example
+for(let j = 0; j < 5; j++)
+{
+    console.log(j);
+}
